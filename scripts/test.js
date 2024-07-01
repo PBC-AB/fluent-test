c6e5168424a2c60f70ecfdf450c60e94cd805e54
Manh-Hai-Bui <manh-hai.bui@fluentcx.com>
2024-07-01 14:51:16 +0200

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,13 @@
 async function initialize(){
 
     try {
-        const response = await fetch('/credentials');
+      
+        const response = await fetch('/credentials', { 
+          method: 'POST', 
+          headers : {'Content-Type' : 'application/json'},
+          body : JSON.stringify({url : window.location.origin + '/'})
+        });
+
         const credentials = await response.text();
     
         console.log('credentials', JSON.stringify(credentials, null, 2));

0a9da44553b08612d1ebd2a327fd82ed94a8a86f
Manh-Hai-Bui <manh-hai.bui@fluentcx.com>
2024-07-01 14:48:15 +0200

    Update credentials.js

--- functions/credentials.js
+++ functions/credentials.js
@@ -1,3 +1,17 @@
-export function onRequest(context){;
-    return new Response(context);
+export async function onRequest(context){;
+
+    const { request } = context;
+
+    if(request.method === 'POST'){
+        const data = await request.json();
+        const envVar = context.env.AUTH_ENDPOINT_BUTTON;
+
+        const responseMessage = `Received: ${JSON.stringify(data)}, Env: ${envVar}`
+
+        return new Response(responseMessage, {
+            headers: {'Content-Type' : 'application/json'},
+        });
+    }
+
+    return new Response('Method not allowed', {status: 405});
 }

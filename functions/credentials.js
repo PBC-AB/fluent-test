bb179724206dae806d7736e70344f62ce156a2de
Manh-Hai-Bui <manh-hai.bui@fluentcx.com>
2024-12-11 16:01:36 +0100

    Update credentials.js

--- functions/credentials.js
+++ functions/credentials.js
@@ -1,43 +1,25 @@
-export async function onRequest(context){;
+export async function onRequest(context){
 
     const { request } = context;
 
     const validURL = context.env.APP_URL_BUTTON; 
 
-    const urlDebug = {
-        validURL : validURL,
-        originURL : "",
-        refererURL : ""
-    };
-
     if(request.method === 'POST'){
         
-        // Get the origin URL from request headers
-        let originURL = request.headers.get('origin');
-        let refererURL = request.headers.get('referer');
+        const authUrl = context.env.AUTH_ENDPOINT_BUTTON;
+        const restUrl = context.env.REST_ENDPOINT_BUTTON;
+        const secretId = context.env.WEBAPPCLIENT_SECRET_BUTTON;
+        const clientId = context.env.WEBAPP_CLIENTID_BUTTON;
 
-        urlDebug.originURL = originURL;
-        urlDebug.refererURL = refererURL;
+        const data = {
+            authEndpoint : authUrl,
+            restEndpoint : restUrl,
+            webAppClientSecret : secretId,
+            webAppClientId : clientId,
+        };
 
-        if(refererURL == validURL){
-
-            const authUrl = context.env.AUTH_ENDPOINT_BUTTON;
-            const restUrl = context.env.REST_ENDPOINT_BUTTON;
-            const secretId = context.env.WEBAPPCLIENT_SECRET_BUTTON;
-            const clientId = context.env.WEBAPP_CLIENTID_BUTTON;
-
-            const data = {
-                authEndpoint : authUrl,
-                restEndpoint : restUrl,
-                webAppClientSecret : secretId,
-                webAppClientId : clientId,
-                urlDebug : urlDebug
-            };
-
-            return new Response(JSON.stringify(data), {headers: {'Content-Type' : 'text/plain'}});
-        }
-
-        return new Response('Conflict:' + JSON.stringify(urlDebug), {status: 409});
+        return new Response(JSON.stringify(data), {headers: {'Content-Type' : 'text/plain'}});
+        
     }
 
     return new Response('Method not allowed', {status: 405});
